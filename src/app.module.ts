fd3b583d717e21ef0d9347f851c9338a05b3b185
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2023-04-24 22:42:15 -0300

    feat: registry classes on AppModule
    - apply exception filters

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,50 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { createClient } from '@supabase/supabase-js';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { WorkerMemoryRepository } from '@worker/infra/repository/memory/WorkerMemory.repository';
+import { RegisterWorkerUseCase } from '@worker/app/useCase/register/RegisterWorker.useCase';
+import { WorkerController } from '@worker/infra/controller/Worker.controller';
+
+import {
+  SUPABASE_CLIENT,
+  WORKER_EVENT_EMITTER,
+  WORKER_REPOSITORY,
+} from '@utils/constants';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController, WorkerController],
+  providers: [
+    AppService,
+    RegisterWorkerUseCase,
+    {
+      provide: SUPABASE_CLIENT,
+      useFactory: (configService: ConfigService) => {
+        const supabaseUrl = configService.get<string>('DB_URL');
+        const supabaseKey = configService.get<string>('SERVICE_ROLE_KEY');
+        return createClient(supabaseUrl, supabaseKey);
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: WORKER_EVENT_EMITTER,
+      useValue: {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        emit: () => {},
+      },
+    },
+    {
+      provide: WORKER_REPOSITORY,
+      useClass: WorkerMemoryRepository,
+    },
+  ],
 })
 export class AppModule {}
